9b1fa8c531cd5f3be2210e1d00d743511359fea6
Ahmad Elhamad <ahmade171@gmail.com>
2017-05-17 14:27:01 -0700

    Update int2primefactor.s

--- Embedded_M68k/int2primefactor.s
+++ Embedded_M68k/int2primefactor.s
@@ -40,21 +40,21 @@ begin:
       	linein		buffer
       	move.l		D0,D4         	* strlen to D4
       	move.l		D0,D3		        * and D3
-      	subq		  #1,D4           * DBRA counter
-      	tst.l		  D0		          * is strlen == 0?
-      	beq		    bad
+      	subq		#1,D4           * DBRA counter
+      	tst.l		D0		          * is strlen == 0?
+      	beq		bad
       	cmpi.l		#5,D0
-      	bhi		    bad
-      	bra		    length      
+      	bhi		bad
+      	bra		length      
       
 again2:   
-	      clr.l		  buffer 
-        BRA		    begin       
+	clr.l		buffer 
+        BRA		begin       
 bad:  
       	lineout		error_msg   * If error occurs, output error message
-     	  clr.l		  buffer      * clear memory buffers
+     	  clr.l		buffer      * clear memory buffers
      	  clr.b	  	buffer+4
-     	  bra		    begin       * and restart       
+     	  bra		begin       * and restart       
       
         * check that all input are digits 0-9      
 length:
@@ -62,25 +62,25 @@ length:
         
 check:      
         cmpi.b		#'0',(A0)    * check if ascii code < 47
-        blo			  bad          * if it is goto bad
+        blo		bad          * if it is goto bad
         cmpi.b		#'9',(A0)+   * check if ascii code > 58
-        bhi		    bad          * if it is, goto bad
-        dbra		  D4,check
+        bhi		bad          * if it is, goto bad
+        dbra		D4,check
       
        * and that value is from 2-65535
       	    
-       cvta2		  buffer,D0
+       cvta2		buffer,D0
        move.l	  	D0,D1         *   and D1
-       move.l		  D0,D2        * copy input value to D2
-       cmpi.l		  #2,D0
-       blo		    bad
-       cmpi.l		  #65535,D0
-       bhi	  	  bad
+       move.l		D0,D2        * copy input value to D2
+       cmpi.l		#2,D0
+       blo		bad
+       cmpi.l		#65535,D0
+       bhi	  	bad
       
       *  ALGORITHM
             
       move.w		#2,D5          * factor stored in D5 with value of 2
-      lea		    solut,A2
+      lea		solut,A2
       * check that factor <= number
       
 FOR: 
@@ -92,25 +92,25 @@ FOR:
       andi.l		#$0000FFFF,D1  
       move.l		D1,D7             * D7 is the input val 
       divu.w		D5,D7           
-      swap		  D7                * D7 % D5
-      tst.w		  D7                * see if number % factor == 0
-      BNE		    ENDIF       
+      swap		D7                * D7 % D5
+      tst.w		D7                * see if number % factor == 0
+      BNE		ENDIF       
       move.w		D5,D0         
       andi.l		#$0000FFFF,D0
       cvt2a	  	(A2),#2
-      stripp	  (A2),#2
+      stripp	  	(A2),#2
       adda.l		D0,A2
       move.b		#'*',(A2)+
-      divu		  D5,D1	* number /= factor
+      divu		D5,D1		* number /= factor
       subq.w		#1,D5
 
-ENDIF:                   * 
+ENDIF:                   
       addq.w		#1,D5
-      BRA		    For      
+      BRA		For      
        
 done: 	 
        
-      clr.b		  -(A2)
+      clr.b		-(A2)
       lineout		facts 
       lineout		solut
       
@@ -120,39 +120,39 @@ done:
 prompt:
       lineout		redo           
       linein		buffer2
-      lea		    buffer2,A1
+      lea		buffer2,A1
       cmpi.b		#1,D0
-      BNE		    prompt
+      BNE		prompt
       cmpi.b		#89,(A1)      * (Y/N)  ?
-      BEQ		    begin
+      BEQ		begin
       cmpi.b		#121,(A1)
-      BEQ		    begin         * makes sure user entered either YyNn
+      BEQ		begin         * makes sure user entered either YyNn
       cmpi.b		#110,(A1)
-      BEQ 		  fin
+      BEQ 		fin
       cmpi.b		#78,(A1)
-      BEQ		    fin
+      BEQ		fin
       lineout		invalid
-      BRA		    prompt
+      BRA		prompt
 	
 fin:  
-	    lineout		terminate
+      lineout		terminate
         
 break                   * Terminate execution
 **----------------------------------------------------------------------
 *       Storage declarations
 
 
-buffer2:	    ds.b		80
-skipln:		    dc.b		0
+buffer2:	ds.b		80
+skipln:		dc.b		0
 title:	    	dc.b		'Program #3, Ahmad Elhamad, masc1096',0
-title2:		    dc.b		'Enter an integer to factor (2..65535):',0
-error_msg:    dc.b		'Sorry, your input is not a valid integer.',0
-invalid:	    dc.b		'Invalid Answer',0
-facts:		    dc.b		'The factors of '
-buffer: 	    ds.b		5
-are:		      dc.b		' are:',0
-solut:		    ds.b		40
-redo:		      dc.b		'Do you want to factor another integer (Y/N)?',0
-terminate:    dc.b		'Program terminated.'
+title2:		dc.b		'Enter an integer to factor (2..65535):',0
+error_msg:  	dc.b		'Sorry, your input is not a valid integer.',0
+invalid:	dc.b		'Invalid Answer',0
+facts:		dc.b		'The factors of '
+buffer: 	ds.b		5
+are:		dc.b		' are:',0
+solut:		ds.b		40
+redo:		dc.b		'Do you want to factor another integer (Y/N)?',0
+terminate:    	dc.b		'Program terminated.'
 
 
