b18037a77045edf02ce6fd58ec246072073f253e
Ahmad Elhamad <ahmade171@gmail.com>
2018-09-25 12:39:36 -0700

    Add files via upload
    Custom script created to facilitate collection of open-source intelligence.

---
+++ IOC_Lookup.py
@@ -0,0 +1,35 @@
+import webbrowser
+
+input_val = input("Enter the IP to lookup: ")
+
+# md5, sha256, sha1
+if len(input_val) in [32, 64, 40]: 
+	url = "https://www.virustotal.com/search/?query=" + input_val
+	webbrowser.open(url,new=2, autoraise=True)
+
+# domain name
+else if "www." in input_val or "http" in input_val or ".com" in input_val:
+	url = "https://urlquery.net/search?q=" + input_val
+	webbrowser.open(url,new=2, autoraise=True)
+
+# ip address
+else:
+	url = "https://www.talosintelligence.com/reputation_center/lookup?search=" + input_val
+	webbrowser.open(url, new=1, autoraise=True)
+
+	url = "https://www.virustotal.com/#/ip-address/" + input_val
+	webbrowser.open(url,new=2, autoraise=True)
+	
+	url = "https://www.shodan.io/host/" + input_val
+	webbrowser.open(url,new=2, autoraise=True)
+
+	url = "https://otx.alienvault.com/browse/indicators?sort=indicator&q=" + input_val
+	webbrowser.open(url,new=2, autoraise=True)
+
+	url = "https://community.riskiq.com/search/" + input_val
+	webbrowser.open(url,new=2, autoraise=True)
+
+	
+
+	
+	
